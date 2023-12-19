f21dca5620dc6b4f81afd5039800fee5c14ef235
AngelArevalo <aarevalo@dvconsultores.com>
2023-12-19 11:57:11 -0400

    gitignore

--- mysite/backend/modulos.py
+++
@@ -1,8 +0,0 @@
-modelos=[
-    {'nombre':'UsuariosDefix'},
-    {'nombre':'Transacciones'},
-    {'nombre':'Balance'},
-    {'nombre':'UsersAdmin'},
-    {'nombre':'Paises'},
-    {'nombre':'Comisiones'}
-]
